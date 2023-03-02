04f72b2f0aa0b0bded20283a65b86cf8bd05bb4c
storezhang <storezhang@gmail.com>
2023-03-02 17:40:23 +0800

    feat(任务): 记录任务执行日志

--- job_default.go
+++ job_default.go
@@ -1,21 +1,33 @@
 package schedule
 
 import (
+	"github.com/goexl/gox"
+	"github.com/goexl/gox/field"
+	"github.com/goexl/simaqian"
 	"github.com/robfig/cron/v3"
 )
 
 var _ cron.Job = (*jobDefault)(nil)
 
 type jobDefault struct {
+	logger simaqian.Logger
 	worker worker
 }
 
-func newDefaultJob(worker worker) *jobDefault {
+func newDefaultJob(logger simaqian.Logger, worker worker) *jobDefault {
 	return &jobDefault{
+		logger: logger,
 		worker: worker,
 	}
 }
 
 func (jd *jobDefault) Run() {
-
+	fields := gox.Fields[any]{
+		field.New("worker", jd.worker),
+	}
+	if err := jd.worker.Run(); nil != err {
+		jd.logger.Warn("任务执行出错", fields.Add(field.Error(err))...)
+	} else {
+		jd.logger.Debug("任务执行成功", fields...)
+	}
 }
--- scheduler_add.go
+++ scheduler_add.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (s *Scheduler) add(params *addParams) (id string, err error) {
-	if eid, ae := s.addTo(params); nil == ae {
+	if eid, ae := s.addToCron(params); nil == ae {
 		id = gox.Ift("" == params.id, xid.New().String(), gox.ToString(eid))
 		s.ids.Store(id, eid)
 	}
@@ -15,10 +15,10 @@ func (s *Scheduler) add(params *addParams) (id string, err error) {
 	return
 }
 
-func (s *Scheduler) addTo(params *addParams) (id cron.EntryID, err error) {
+func (s *Scheduler) addToCron(params *addParams) (id cron.EntryID, err error) {
 	switch params.typ {
 	case typeCron, typeDuration, typeFixed:
-		id, err = s.cron.AddJob(params.ticker.tick(), newDefaultJob(params.worker))
+		id, err = s.cron.AddJob(params.ticker.tick(), newDefaultJob(s.logger, params.worker))
 	}
 
 	return
