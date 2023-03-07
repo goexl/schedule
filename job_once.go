a70d02aaff6e7add5363f48edd227d2f97e911f4
storezhang <storezhang@gmail.com>
2023-03-07 21:38:39 +0800

    feat(任务): 任何情况下，确保一次性任务被正确的删除

--- job_once.go
+++ job_once.go
@@ -11,7 +11,9 @@ type jobOnce struct {
 	id     *cron.EntryID
 	cron   *cron.Cron
 	worker   worker
+
 	logger   simaqian.Logger
+	deleted bool
 }
 
 func newOnceJob(id *cron.EntryID, cron *cron.Cron, worker worker, logger simaqian.Logger) *jobOnce {
@@ -19,13 +21,14 @@ func newOnceJob(id *cron.EntryID, cron *cron.Cron, worker worker, logger simaqia
 		id:id,
 		cron:cron,
 		worker:   worker,
+
 		logger:   logger,
 	}
 }
 
 func (jo *jobOnce) Run() {
-	// 删除原来的任务，确保不会再被执行
-	defer jo.cron.Remove(*jo.id)
+	// 在任何情况下，确保任务被删除
+	defer jo.cleanup()
 
 	fields := gox.Fields[any]{
 		field.New("worker", jo.worker),
@@ -34,5 +37,15 @@ func (jo *jobOnce) Run() {
 		jo.logger.Warn("任务执行出错", fields.Add(field.Error(err))...)
 	} else {
 		jo.logger.Debug("任务执行成功", fields...)
+	}
+
+	// 删除原来的任务，确保不会再被执行
+	jo.cron.Remove(*jo.id)
+	jo.deleted = true
+}
+
+func (jo *jobOnce) cleanup() {
+	if !jo.deleted {
+		jo.cron.Remove(*jo.id)
 	}
 }
